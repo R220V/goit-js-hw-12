c97e9fb5a46774fc6ea3559197f91fe1545d196c
Krus <krus.margo@gmail.com>
2025-02-02 03:06:15 +0200

    hw12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,37 +1,20 @@
 import axios from 'axios';
 
-export const fetchPhotosByQuery = (searchedQuery, currentPage) => {
+const API_KEY = '48306389-9c3f7e9b102fd2bc2270acf47';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export function fetchPhotosByQuery(query, currentPage) {
   const axiosOptions = {
     params: {
-      q: searchedQuery,
-      key: '48306389-9c3f7e9b102fd2bc2270acf47',
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: 'true',
+      key: API_KEY,
       page: currentPage,
       per_page: 15,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
     },
   };
-  return axios.get(`https://pixabay.com/api/`, axiosOptions);
-};
-// ============================================
-// export const fetchPhotosByQuery = searchedQuery => {
-//   const searchParams = new URLSearchParams({
-//     q: searchedQuery,
-//     key: '48306389-9c3f7e9b102fd2bc2270acf47',
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//     safesearch: 'true',
-//     page: 1,
-//     per_page: '15',
-//   });
 
-//   return fetch(`https://pixabay.com/api/?${searchParams.toString()}`).then(
-//     response => {
-//       if (!response.ok) {
-//         throw new Error(response.status);
-//       }
-//       return response.json();
-//     }
-//   );
-// };
+  return axios.get(`${BASE_URL}`, axiosOptions);
+}
--- src/main.js
+++ src/main.js
@@ -1,44 +1,44 @@
-import iziToast from 'izitoast'; // Завантажимо бібліотеки
+// Завантажимо бібліотеки
+import iziToast from 'izitoast';
 import SimpleLightbox from 'simplelightbox';
 
-import { createGalleryCardTemplate } from './js/render-functions';
 import { fetchPhotosByQuery } from './js/pixabay-api';
+import { createGalleryCardTemplate } from './js/render-functions';
 
 // Отримуємо посилання на елементи сторінки
-const searchFormEl = document.querySelector('.js-form-search');
-const galleryEl = document.querySelector('.js-gallery');
-const loader = document.querySelector('.loader');
-const loadMoreBtn = document.querySelector('.load-more-btn');
+const formSearchCard = document.querySelector('.form-search');
+const listGalleryCard = document.querySelector('.gallery');
+const loaderStyle = document.querySelector('.loader');
+const buttonLoadEl = document.querySelector('.button-load');
+
+let page = 1; //номер сторінки результатів пошуку. Починаємо з 1.
+let query = ''; //зберігає ключове слово, яке ввів користувач у формі.
+
+loaderStyle.style.display = 'none'; //сховаємо лоадер
+buttonLoadEl.classList.add('is-hidden'); //сховаємо кнопку Load More
 
 //Ініціалізація SimpleLightbox- створення модального вікна,
 //(зображення відкривається у великому розмірі).
-let gallerySimpleLightbox = new SimpleLightbox('.gallery a', {
+const lightboxModalWindow = new SimpleLightbox('.gallery a', {
+  captions: true,
   captionsData: 'alt',
-  captionDelay: 300,
+  captionsPosition: 'bottom',
+  captionsDelay: 500,
 });
 
-//номер сторінки результатів пошуку. Починаємо з 1.
-let page = 1;
-
-//зберігає ключове слово, яке ввів користувач у формі.
-let searchedQuery = '';
-
 //ф-ія викликається при сабміті форми
-const onSearchFormSubmit = async event => {
-  // Запобігаємо перезавантаженню сторінки
-  event.preventDefault();
-
+const searchImg = async event => {
   try {
-    //сховаємо кнопку Load More
-    // loadMoreBtn.classList.add('hidden');
-    // loader.style.display = 'none';
+    event.preventDefault(); // Запобігаємо перезавантаженню сторінки
 
-    //зчитуємо значення з інпута, видаляючи пробіли
-    searchedQuery = event.currentTarget.elements.user_query.value.trim('');
+    query = event.target.query.value.trim(); //зчитуємо значення з інпута, видаляючи пробіли
+    //відмалювали картки в галереї
+    listGalleryCard.innerHTML = ''; //почистили галерею
+
     //перевірка, чи інпут не порожній
-    if (searchedQuery === '') {
+    if (!query) {
       iziToast.error({
-        title: '',
+        title: 'Error',
         messageColor: 'Purple',
         color: 'red',
         position: 'topRight',
@@ -47,20 +47,16 @@ const onSearchFormSubmit = async event => {
       });
       return;
     }
-    // відкриємо 1 сторінку при кожному пошуку
-    // page = 1;
 
-    //сховаємо кнопку Load More
-    // loadMoreBtn.classList.add('hidden');
-    // //Видалимо клас is-hidden для показу індикатора завантаження
-    // loader.classList.remove('is-hidden');
+    page = 1; // відкриємо 1 сторінку при кожному пошуку
 
-    //Зробили запит поключовому слову searchedQuery
-    const { data } = await fetchPhotosByQuery(searchedQuery, page);
+    buttonLoadEl.classList.add('is-hidden'); //сховаємо кнопку Load More
 
-    //перевірка на неіснуюче слово в інпуті
+    //Зробили запит по ключовому слову query
+    const { data } = await fetchPhotosByQuery(query, page);
+
     //Якщо на сервері немає зображень за таким пошуком, відображається сповіщення про помилку.
-    if (data.total === 0) {
+    if (data.hits.length === 0) {
       iziToast.error({
         title: '',
         messageColor: 'Purple',
@@ -69,171 +65,74 @@ const onSearchFormSubmit = async event => {
         messageSize: '20',
         message: 'Sorry, there are no images. Please try again!',
       });
-      galleryEl.innerHTML = ''; //почистили галерею
-      searchFormEl.reset(); //почистили імпут
       return;
     }
-    // якщо на сервері > 1 групи зображень
-    // if (data.totalHits > 1) {
-    //   loadMoreBtn.classList.remove('hidden');
-    // слухаємо подію клік на кнопці, якщо >1 групи зображень
-    // loadMoreBtn.addEventListener('click'.onLoadMoreBtnClick);
 
-    //генеруємо розмітку(якщо є зображення, формуємо рядок з розміткою)
-    const galleryTemplate = data.hits
-      .map(el => createGalleryCardTemplate(el))
-      .join('');
-    //відмалювали картки в галереї
-    galleryEl.innerHTML = galleryTemplate;
-    //Покажемо кнопку load-more --приберемо клас hidden
-    loadMoreBtn.classList.remove('hidden');
-
-    //слухаємо подію клік на кнопці
-    loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
-  } catch (err) {
-    console.log(err);
+    //домалювали картки в кінець галереї
+    listGalleryCard.insertAdjacentHTML(
+      'beforeend',
+      createGalleryCardTemplate(data.hits)
+    );
+    // якщо карток більше 1 сторінки, покажемо кнопку Lode More
+    if (data.totalHits > page * 15) {
+      buttonLoadEl.classList.remove('is-hidden');
+    }
+    lightboxModalWindow.refresh(); //оновлюємо галерею SimpleLightbox
+    event.preventDefault();
+  } catch (error) {
+    console.error(error.message);
+  } finally {
+    loaderStyle.style.display = 'none'; //ховаємо лоадер
   }
+  //очистимо форму після завершення запиту
+  event.target.reset();
 };
-// Додаємо обробник події на форму пошуку
-searchFormEl.addEventListener('submit', onSearchFormSubmit);
 
-// опишемо ф-ію обробнику кліку
-const onLoadMoreBtnClick = async event => {
-  console.log('object');
+// Додаємо обробник події на форму пошуку
+formSearchCard.addEventListener('submit', searchImg);
+
+// опишемо ф-ію обробника кліка
+const onButtonLoadClick = async () => {
   try {
     page++;
-    // робимо запит по ключовому слову searchedQuery
-    const { data } = await fetchPhotosByQuery(searchedQuery, page);
-    console.log(data);
-    //генеруємо розмітку(якщо є зображення, формуємо рядок з розміткою)
-    const galleryTemplate = data.hits
-      .map(el => createGalleryCardTemplate(el))
-      .join('');
-    //
-    console.log(galleryTemplate);
+    const { data } = await fetchPhotosByQuery(query, page);
+
     //додаємо нові зображення в кінець нашого списку
-    galleryEl.insertAdjacentHTML('beforeend', galleryTemplate);
-    //     //коли настає кінець колекції
-    //     if (page === data.totalHits) {
-    //       // ховаэмо кнопку
-    //       loadMoreBtnEl.classList.add('is-hidden');
-    //       //припиняємо прослуховування на кнопці
-    //       loadMoreBtnEl.removeEventListener('click', onLoadMoreBtnClick);
-  } catch (err) {
-    console.log(err);
+    listGalleryCard.insertAdjacentHTML(
+      'beforeend',
+      createGalleryCardTemplate(data.hits)
+    );
+
+    //коли настає кінець колекції
+    if (page * 15 >= data.totalHits || data.hits.length === 0) {
+      iziToast.info({
+        title: 'Info',
+        messageColor: 'teal',
+        color: 'blue',
+        position: 'topRight',
+        messageSize: '20',
+        message: "We're sorry, but you've reached the end of search results.",
+      });
+
+      buttonLoadEl.classList.add('is-hidden'); // ховаємо кнопку
+      return;
+    }
+    //реалізуєио плавний скрол на 2 карточки
+    const smoothScroll = document.querySelector('.gallery-item');
+    if (smoothScroll) {
+      const cardHeight = smoothScroll.getBoundingClientRect().height;
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
+    }
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      position: 'topRight',
+      message: 'Please try again later.',
+    });
   }
 };
-
-// iziToast.err({
-//   title: 'Error',
-//   position: 'topRight',
-//   message: 'The image you requested could not be loaded. Please try again later.',
-
-// });
-
-//       const galleryTemplate = data.results
-//         .map(el => createGalleryCardTemplate(el))
-//         .join('');
-
-//
-//
-
-//         iziToast.info({
-//           title: 'Info',
-//           position: 'topRight',
-//           message: "There are no more images matching your request.",
-//         });
-//
-
-//
-//       }
-//     }
-//     const smoothScroll = document.querySelector('.gallery-item');
-//     if(smoothScroll) {
-//      const cardHeight = smoothScroll.getBoundingClientRect().height;
-//       window.scrollBy({
-//         top: cardHeight * 2,
-//         behavior: 'smooth',
-//       });
-//     }
-
-// ===================================================================
-
-//--------------------------------------------------------------------------------------------------------------
-// // Завантажимо бібліотеки
-
-// import iziToast from 'izitoast';
-// import SimpleLightbox from 'simplelightbox';
-
-// import { createGalleryCardTemplate } from './js/render-functions';
-// import { fetchPhotosByQuery } from './js/pixabay-api';
-// // Отримуємо посилання на елементи сторінки
-// const searchFormEl = document.querySelector('.js-form-search');
-// const galleryEl = document.querySelector('.js-gallery');
-// const loader = document.querySelector('.loader');
-// // Ініціалізація SimpleLightbox
-// let gallerySLB = new SimpleLightbox('.gallery a', {
-//   captionsData: 'alt',
-//   captionDelay: 300,
-// });
-// //ф-ія викликається при сабміті форми
-// const onSearchFormSubmit = event => {
-//   // Запобігаємо перезавантаженню сторінки
-//   event.preventDefault();
-//   //зчитуємо значення з інпута, видаляючи пробіли
-//   const searchedQuery = event.currentTarget.elements.user_query.value.trim('');
-//   //перевірка, чи інпут не порожній
-//   if (searchedQuery === '') {
-//     iziToast.error({
-//       title: 'Error',
-//       messageColor: 'Purple',
-//       color: 'red',
-//       position: 'topRight',
-//       message: 'Please enter your request',
-//       messageSize: '20',
-//     });
-//     return;
-//   }
-//   //Видалимо клас is-hidden для показу індикатора завантаження
-//   loader.classList.remove('is-hidden');
-//   fetchPhotosByQuery(searchedQuery) //запит на сервер
-//     .then(data => {
-//       //перевірка на неіснуюче слово в інпуті
-//       //Якщо на серверінемає зображень за таким пошуком, відображається сповіщення про помилку.
-//       if (data.total === 0) {
-//         iziToast.error({
-//           title: 'Error',
-//           messageColor: 'Purple',
-//           color: 'red',
-//           position: 'topRight',
-//           messageSize: '20',
-//           message: 'Sorry, there are no images. Please try again!',
-//         });
-//         galleryEl.innerHTML = ''; //почистили галерею
-//         searchFormEl.reset(); //почистили імпут
-//         return;
-//       }
-//       //генеруємо розмітку(якщо є зображення, формуємо рядок з розміткою)
-//       const galleryTemplate = data.hits
-//         .map(el => createGalleryCardTemplate(el))
-//         .join('');
-//       //відмальовуємо зображення на сторінці
-//       galleryEl.innerHTML = galleryTemplate;
-//       //ховаємо індикатор завантаження
-//       loader.classList.add('is-hidden');
-//       //оновлюємо галерею SimpleLightbox
-//       gallerySLB.refresh();
-//     })
-//     //Якщо є помилка, ховається індикатор завантаження, а помилка виводиться в консоль.
-//     .catch(err => {
-//       loader.style.display = 'none';
-//       console.log(err);
-//     });
-//   //очистимо форму після завершення запиту
-//   searchFormEl.reset();
-// };
-
-// // Додаємо обробник події на форму пошуку
-// searchFormEl.addEventListener('submit', onSearchFormSubmit);
-
-// ====================================
+//слухаємо подію клік на кнопці
+buttonLoadEl.addEventListener('click', onButtonLoadClick);
